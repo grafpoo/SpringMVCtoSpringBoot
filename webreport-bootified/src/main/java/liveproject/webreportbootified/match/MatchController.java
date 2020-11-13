40d340a853eea3e72d3c64bfa8dc4c803affe8b8
john <grafpoo@gmail.com>
2020-11-13 15:29:39 -0500

    bootified result

---
+++ webreport-bootified/src/main/java/liveproject/webreportbootified/match/MatchController.java
@@ -0,0 +1,29 @@
+package liveproject.webreportbootified.match;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/reports")
+public class MatchController {
+
+    private MatchService matchService;
+
+    @Autowired
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping("season-report/{season}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String account(@PathVariable("season") String season, Model model) {
+        model.addAttribute("season", matchService.aggregateSeason(season));
+        return "reports/SeasonReport";
+    }
+}
---
+++ webreport-bootified/src/test/java/liveproject/webreportbootified/WebreportBootifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package liveproject.webreportbootified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebreportBootifiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
